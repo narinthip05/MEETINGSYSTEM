618e3123141ef756865dddd0cfee634d3e67af35
narinthip05 <narinthipying335577@gmail.com>
2025-01-24 11:18:18 +0700

    ...

--- backend/model/users.go
+++ backend/model/users.go
@@ -1,7 +1,7 @@
 package model
 
 type Users struct {
-	Userid   int    `json:"user_id"`       //รหัสผู้ใช้งาน
+	Userid    int    `json:"user_id"`       //รหัสผู้ใช้งาน
 	Username  string `json:"username"`      //ชื่อผู้ใช้งาน
 	Password  string `json:"password"`      //รหัสผ่าน
 	Role      int    `json:"role"`			//ตำแหน่ง (Admin,Users)
