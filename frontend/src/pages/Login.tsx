dab5132e5f3f6009cf3d82efde4086031338bf29
narinthip05 <narinthipying335577@gmail.com>
2025-01-24 12:52:38 +0700

    ..

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -14,55 +14,53 @@ const Login: React.FC = () => {
 
   return (
     <div className="body-login">
-    <div className="login-container">
-      <img src="src/assets/SingleLogo.png" alt="Logo" className="logo" style={{width: "150px"}}/>
-      <div className="login-box">
-      <h2>ยินดีตอนรับเข้าสู่ระบบ</h2>
-        <form onSubmit={handleSubmit} className="login-form">
-          <label htmlFor="username">ชื่อผู้ใช้ (Username)</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="ชื่อผู้ใช้"
-            required
-          />
+      <div className="login-container">
+        <img src="src/assets/SingleLogo.png" alt="Logo" className="logo" style={{ width: "150px" }} />
+        <div className="login-box">
+          <h3>ยินดีตอนรับเข้าสู่ระบบ</h3>
+          <form onSubmit={handleSubmit} className="login-form">
+            <label htmlFor="username">ชื่อผู้ใช้ (Username)</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="ชื่อผู้ใช้"
+              required
+            />
 
-          <label htmlFor="password">รหัสผ่าน (Password)</label>
-          <div style={{ display: "flex", alignItems: "center" }}></div>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="รหัสผ่าน"
-            required
-          />
-          <div className="remember-forgot">
-  <div className="remember-me">
-    <input
-      type="checkbox"
-      id="rememberMe"
-      checked={rememberMe}
-      onChange={() => setRememberMe(!rememberMe)}
-    />
-    <label htmlFor="rememberMe" className="remember-label">
-    จำไว้
-    </label>
-  </div>
-  <a href="/forgot-password" className="forgot-password">
-    ลืมรหัสผ่าน?
-  </a>
-</div>
-
-
-          <button type="submit">เข้าสู่ระบบ</button>
-        </form>
+            <label htmlFor="password">รหัสผ่าน (Password)</label>
+            <div style={{ display: "flex", alignItems: "center" }}></div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="รหัสผ่าน"
+              required
+            />
+            <div className="remember-forgot">
+              <div className="remember-me">
+                <input
+                  type="checkbox"
+                  id="rememberMe"
+                  checked={rememberMe}
+                  onChange={() => setRememberMe(!rememberMe)}
+                />
+                <label htmlFor="rememberMe" className="remember-label">
+                  จำไว้
+                </label>
+              </div>
+              <a href="/forgot-password" className="forgot-password">
+                ลืมรหัสผ่าน?
+              </a>
+            </div>
+            <button type="submit">เข้าสู่ระบบ</button>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
